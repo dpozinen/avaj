895ec706c9a542678ed0d6dfef90bfea6efeac2e
Dariy Pozinenko <dpozinen@gmail.com>
2019-01-19 12:54:06 +0200

    started testing

---
+++ src/test/java/dpozinen/tests/MainTest.java
@@ -0,0 +1,12 @@
+package dpozinen.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+	@Test
+	public void testDivide() {
+		assertEquals(42, Integer.sum(19, 23));
+	}
+}
